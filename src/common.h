85e0cae4dda10d821c2c63b64a4e26e7518ede55
SeongJin Jang <me@seongjxn.page>
2024-12-10 17:31:03 +0900

    [ADD] common.h
    - wiringPi 라이브러리 불러오기
    - MessageQueue 라이브러리 불러오기

---
+++ src/common.h
@@ -0,0 +1,6 @@
+#include <stdio.h>            // 표준입출력용 라이브러리  
+#include <stdlib.h>           // 표준 유틸리티용 라이브러리
+#include <stdint.h>           // 정수 자료형 라이브러리
+
+#include <wiringPi.h>         // wiringPi 라이브러리 사용
+#include <mqueue.h>           // MessageQueue
